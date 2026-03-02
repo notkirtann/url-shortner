6333ec6a5ec95934a4511950e0f71070bf26a5fa
kirtannn1 <119801478+notkirtann@users.noreply.github.com>
2026-03-02 12:50:48 +0530

    swagger documentation improvement changing swagger.ts to swagger.yaml

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import express from 'express'
 import type {Request, Response} from 'express'
+import cors from 'cors';
 import chalk from 'chalk'
 import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './utils/swagger.ts';
+import { resolve } from 'path';
+import YAML from 'yamljs';
 import {userRoutes,urlRoutes} from './routes/routes.ts'
 import { authenticationMiddleware } from './middleware/auth.middleware.ts'
 
@@ -11,10 +13,15 @@ const app = express()
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 app.use(express.json())
+app.use(cors())
 app.use(authenticationMiddleware)
 
 app.use('/user',userRoutes)
+
+//swagger
+const swaggerSpec = YAML.load(resolve('./swagger.yaml'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use(urlRoutes)
 
 //health check route
